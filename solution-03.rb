cd46be65bf4fe99e5c4a58e098eb01d692100bf4
Shakeel Rauf <shakeel.rauf@healthwire.pk>
2022-06-30 12:53:53 +0500

    Solution of part 1 question 3.

---
+++ solution-03.rb
@@ -0,0 +1,14 @@
+#only generating  nth numeber series to find nth.
+def fibonnaci number
+  default_series = [0,1]
+  length_of_series = default_series.length
+  return number if default_series.include? number
+  (0..number).each do |nth_number|
+    last_number = default_series[length_of_series-1]
+    second_last_number = default_series[length_of_series-2]
+    default_series.push(last_number + second_last_number)
+    
+    length_of_series = default_series.length
+  end
+  default_series[number]
+end
