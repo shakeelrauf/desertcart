39c5099524eddb7bbcdc205cdbfde8dc6c52d1a1
shakeel rauf <shkeelrauf@gmail.com>
2022-06-30 14:28:26 +0500

    Update solution-01.rb

--- solution-01.rb
+++ solution-01.rb
@@ -3,10 +3,10 @@ def reverse string
   splitted_string_array = string.split('')
   length_of_string = string.length
   i = 0
-  while( i < length_of_string / 2) do
+  while( i <= (length_of_string / 2)) do
     temp = splitted_string_array[i]
-    splitted_string_array[i] = splitted_string_array[length_of_string - i]
-    splitted_string_array[length_of_string - i] = temp
+    splitted_string_array[i] = splitted_string_array[length_of_string - 1 - i]
+    splitted_string_array[length_of_string -1 - i] = temp
     i+=1
   end
   splitted_string_array.join('')
